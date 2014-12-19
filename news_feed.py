52d8ad4b8f92f9fba23e07b309a0411dfd437c5d
MALAKAGL <glmalaka@gmail.com>
2014-12-19 14:32:14 +0530

    Updated Script

--- news_feed.py
+++ news_feed.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
-import urllib , json , smtplib , sys , logging
-import ConfigParser
+import urllib , json , smtplib , sys , logging , ConfigParser
 import sched
 import time
 import os
