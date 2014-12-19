89a183fd8e5c9f47461d983af17a6977efc9f541
U-ALLION\lahiru <lahiru@AT-DTDEV25.alliontechnologies.local>
2014-12-19 16:32:23 +0530

    Cleared.

--- scheduler.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-nohup python news_feed.py > /dev/null 2>&1 & echo $! > running_pid
-while true;
-do
-	echo "Scheduler started..."
-	./git_pull.sh
-	echo "Scheduler sleeping..."
-	sleep 20 
-done
